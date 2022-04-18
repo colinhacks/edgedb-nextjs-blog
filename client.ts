94e90843b819b9eeb83fea9ad79f44777fedf2a3
Colin McDonnell <colin@edgedb.com>
2022-04-17 17:38:17 -0700

    Remove hard coded creds

--- client.ts
+++ client.ts
@@ -2,10 +2,5 @@ import { createClient } from 'edgedb'
 import e from './dbschema/edgeql-js'
 
 // reads value of EDGEDB_DSN automatically
-export const client = createClient({
-  // TLS configuration is beyond the scope
-  // of this example project
-  dsn: 'edgedb://edgedb:edgedbpassword@157.245.77.5:5656/edgedb',
-  tlsSecurity: 'insecure',
-})
+export const client = createClient()
 export { e }
