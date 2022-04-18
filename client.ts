e20d80f50d30ea1038e8a72205dc3cb065586269
Colin McDonnell <colin@edgedb.com>
2022-04-18 15:55:17 -0700

    Require TLS checks

--- client.ts
+++ client.ts
@@ -4,6 +4,6 @@ import e from './dbschema/edgeql-js'
 // reads value of EDGEDB_DSN automatically
 export const client = createClient({
   // TLS configuration is beyond the scope of this example project
-  tlsSecurity: 'insecure',
+  // tlsSecurity: 'insecure',
 })
 export { e }
